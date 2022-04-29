9eb2ed9c4d95ca9f09fd2df9d5070b619df4f509
NathanCarr87 <carr.excel@gmail.com>
2022-04-29 15:19:15 -0600

    1 file modified

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ const level = {
   colors: ['red', 'green', 'blue', 'yellow', 'orange', 'purple'],
 };
 
-let cells = [];
-
 let spriteSize = 64;
 let canvas;
 let ctx;
 const bunches = [];
+const STATES = ['INIT', 'UPDATING', 'NEEDS_UPDATE', 'READY'];
+const CURRENT_STATE = 'INIT';
 window.addEventListener('DOMContentLoaded', (event) => {
   canvas = document.querySelector('canvas');
   ctx = canvas.getContext('2d');
@@ -19,116 +19,121 @@ window.addEventListener('DOMContentLoaded', (event) => {
     console.log(e.offsetX);
     console.log(e.offsetY);
   });
+  STATE = 'INIT';
   draw();
-  duplicateCheck();
 });
 
 class Cell {
   constructor(x, y, color, row) {
-    this.x = x;
+    this.x = x * spriteSize;
     this.y = y;
     this.color = color;
-    this.row = row;
+    this.row = row + 1;
   }
   vy = 4;
+  remove = false;
   draw() {
-    ctx.fillRect(this.x, this.y, spriteSize, spriteSize);
-    ctx.strokeRect(this.x, this.y, spriteSize, spriteSize);
     ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, spriteSize, spriteSize);
+
+    if (this.remove) {
+      ctx.shadowColor = '#fff';
+      ctx.shadowBlur = 20;
+      ctx.lineJoin = 'bevel';
+      ctx.lineWidth = 5;
+      ctx.strokeStyle = '#fff';
+      ctx.strokeRect(this.x, this.y, spriteSize, spriteSize);
+      ctx.shadowColor = 'rgba(0, 0, 0, 0)';
+      ctx.lineWidth = 1;
+      ctx.shadowBlur = 0;
+      ctx.lineJoin = 'miter';
+      ctx.strokeStyle = '#000';
+    } else {
+      ctx.strokeRect(this.x, this.y, spriteSize, spriteSize);
+    }
     ctx.fill();
   }
 }
 
-for (let j = 0; j < 8; j++) {
+for (let j = 0; j <= 7; j++) {
   level.grid[j] = [];
-  for (let i = 0; i < 8; i++) {
+  for (let i = 0; i <= 7; i++) {
     let colorId = Math.floor(Math.random() * level.colors.length);
-    level.grid[j][i] = colorId;
-    cells.push(new Cell(i * spriteSize, i, level.colors[colorId], j));
+    let newCell = new Cell(i, j, level.colors[colorId], j);
+    level.grid[j].push(newCell);
   }
 }
-console.log(level.grid);
+level.grid.reverse();
+// console.log(level.grid)
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  cells.forEach((cell) => {
-    cell.draw();
-    cell.y += cell.vy;
+  level.grid.map((row) => {
+    row.map((cell) => {
+      cell.draw();
+      cell.y += cell.vy;
 
-    if (cell.y + cell.vy > canvas.height - spriteSize * cell.row) {
-      cell.y = spriteSize * level.columns - spriteSize * cell.row;
-    }
+      if (cell.y + cell.vy > canvas.height - spriteSize * cell.row) {
+        cell.y = spriteSize * level.columns - spriteSize * cell.row;
+      }
+    });
   });
+
+  if (STATE === 'INIT') {
+    bunchCheck();
+  }
+  if (STATE === 'NEEDS_UPDATE') {
+    update();
+  }
+  STATE = 'READY';
+
   raf = window.requestAnimationFrame(draw);
 }
 
 function update() {
+  console.log('Updating');
   if (bunches.length > 0) {
-    console.log('do something');
+    bunches.forEach((bunch) => {
+      if (bunch.direction === 'COLUMN') {
+        console.log(
+          'column',
+          bunch,
+          level.grid[bunch.lastColumnIndex][bunch.column]
+        );
+        level.grid[bunch.lastColumnIndex][bunch.column].remove = true;
+        for (let i = 1; i < bunch.size; i++) {
+          level.grid[bunch.lastColumnIndex - i][bunch.column].remove = true;
+        }
+        // level.grid[bunch.lastColumnIndex].splice(bunch.column, 1)
+      } else {
+        console.log('row', bunch);
+      }
+    });
   }
 }
 
-function duplicateCheck() {
-  // check rows
+function bunchCheck() {
+  console.log('Checking for Bunches');
+  // check Columns
   for (let j = 0; j < level.rows; j++) {
-    let tempRow = [];
+    let tempCol = [];
     for (let i = 0; i < level.grid.length; i++) {
-      tempRow.push(level.grid[i][j]);
+      tempCol.push(level.grid[i][j]);
     }
     for (let c = 0; c < level.colors.length; c++) {
-      if (tempRow.filter((x) => x === c).length >= 3) {
+      if (tempCol.filter((x) => x.color === level.colors[c]).length >= 3) {
         let matchCount = 0;
-        for (let z = 0; z < tempRow.length; z++) {
-          if (tempRow[z] === c) {
+        for (let z = 0; z < tempCol.length; z++) {
+          if (tempCol[z].color === level.colors[c]) {
             matchCount++;
           } else {
             if (matchCount >= 3) {
               //Do something with bunch
               bunches.push({
                 size: matchCount,
-                direction: 'HORIZONTAL',
-                lastHorizonatlIndex: z - 1,
-                row: j,
-                type: c,
-              });
-            }
-            matchCount = 0;
-          }
-          if (z === level.rows - 1) {
-            if (matchCount >= 3) {
-              //Do something with the bunch
-
-              bunches.push({
-                size: matchCount,
-                direction: 'HORIZONTAL',
-                lastHorizonatlIndex: z,
-                row: j,
-                type: c,
-              });
-            }
-          }
-        }
-      }
-      // if we don't have more than 3 of a type we can continue
-    }
-  }
-  // Checking for columns
-  for (let i = 0; i < level.grid.length; i++) {
-    for (let c = 0; c < level.colors.length; c++) {
-      // first lets see if we have more than 3 of a type
-      if (level.grid[i].filter((x) => x === c).length >= 3) {
-        let matchCount = 0;
-        for (let z = 0; z < level.grid.length; z++) {
-          if (level.grid[i][z] === c) {
-            matchCount++;
-          } else {
-            if (matchCount >= 3) {
-              //Do something with the bunch
-              bunches.push({
-                size: matchCount,
-                direction: 'VERTICAL',
-                lastVerticalIndex: z - 1,
-                row: i,
+                direction: 'COLUMN',
+                lastColumnIndex: z - 1,
+                column: j,
                 type: c,
               });
             }
@@ -137,11 +142,12 @@ function duplicateCheck() {
           if (z === level.columns - 1) {
             if (matchCount >= 3) {
               //Do something with the bunch
+
               bunches.push({
                 size: matchCount,
-                direction: 'VERTICAL',
-                lastVerticalIndex: z,
-                row: i,
+                direction: 'COLUMN',
+                lastColumnIndex: z,
+                column: j,
                 type: c,
               });
             }
@@ -151,6 +157,49 @@ function duplicateCheck() {
       // if we don't have more than 3 of a type we can continue
     }
   }
-
-  console.log(bunches);
+  // Checking rows
+  // for (let i = 0; i < level.grid.length; i++) {
+  //     for (let c = 0; c < level.colors.length; c++) {
+  //         // first lets see if we have more than 3 of a type
+  //         if (level.grid[i].filter((x) => x.color === level.colors[c]).length >= 3) {
+  //             let matchCount = 0;
+  //             for (let z = 0; z < level.grid.length; z++) {
+  //                 if (level.grid[i][z].color === level.colors[c]) {
+  //                     matchCount++;
+  //                 } else {
+  //                     if (matchCount >= 3) {
+  //                         //Do something with the bunch
+  //                         bunches.push({
+  //                             size: matchCount,
+  //                             direction: 'ROW',
+  //                             lastRowIndex: z - 1,
+  //                             row: i,
+  //                             type: c,
+  //                         });
+  //                     }
+  //                     matchCount = 0;
+  //                 }
+  //                 if (z === level.columns - 1) {
+  //                     if (matchCount >= 3) {
+  //                         //Do something with the bunch
+  //                         bunches.push({
+  //                             size: matchCount,
+  //                             direction: 'ROW',
+  //                             lastRowIndex: z,
+  //                             row: i,
+  //                             type: c,
+  //                         });
+  //                     }
+  //                 }
+  //             }
+  //         }
+  // if we don't have more than 3 of a type we can continue
+  // }
+  // }
+  console.log('bunches', bunches);
+  if (bunches.length > 0) {
+    STATE = 'NEEDS_UPDATE';
+  } else {
+    STATE = 'READY';
+  }
 }
